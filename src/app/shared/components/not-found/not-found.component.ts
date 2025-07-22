41d271d580d540142e38b8fb050ffe9ae6ad2171
Nafis Rubio <nafis@bigordr.com>
2025-07-22 22:42:18 +0800

    Refactor NotFoundComponent with Angular Material and improved design: - Added Material components (MatCard, MatButton, etc.) for layout and styling. - Updated HTML to include 404 error message and navigation link. - Enhanced UI with responsive and accessible design.

--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {MatDivider} from "@angular/material/divider";
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, MatCardActions, MatButton, MatCardHeader, MatCard, MatCardTitle, MatCardContent, MatDivider],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
